01534948b8467c5528405896bad6ee7b5fbfe09b
ziyujiang6 <ziyu18895@gmail.com>
2025-10-20 14:53:57 +0200

    types

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 dataset = openml.datasets.get_dataset(46938)
 df, *_ = dataset.get_data()
 
-# DataInspectation
+# DataIspectation
 df.info()
 
 print(df.describe())
@@ -21,6 +21,47 @@ print(f"Duplicates found: {duplicates}")
 df = pd.get_dummies(df, drop_first=True)
 print(df.head())
 
-df=df.astype(int)
 
-df.info()
+# Select numeric columns (int or float)  #there are also uint8 and category types after get_dummies
+numeric_columns = df.select_dtypes(include=['int64', 'float64','uint8','category']).columns.tolist()
+
+# Select non-numeric columns 
+non_numeric_columns = df.select_dtypes(exclude=['int64', 'float64','uint8','category']).columns.tolist()
+
+# Convert all numeric columns to int64
+df= df.astype(int)
+
+print(df.info)
+
+print(" Numerical Columns:")
+for col in numeric_columns:
+    print(" -", col)
+
+print("\n Non-Numerical (Categorical) Columns:")
+for col in non_numeric_columns:
+    print(" -", col)
+
+
+
+# Standardize numeric columns
+
+
+# scaler = StandardScaler()
+# df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
+
+# SQL exploratory queries
+pysqldf = lambda q: sqldf(q, globals())
+query1 = """
+SELECT region_1, AVG(credit_amount) AS avg_credit
+FROM df
+GROUP BY region_1
+"""
+result1 = pysqldf(query1)
+print(result1)
+
+plt.bar(result1['region_1'], result1['avg_credit'])
+plt.title('Average Credit Amount by Region')
+plt.xlabel('Regions 1 and 2')
+plt.ylabel('Average Credit Amount')
+plt.show()
+
