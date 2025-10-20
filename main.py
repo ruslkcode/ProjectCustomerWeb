ac50a69ffe294c07ff3b6853033f28e10b0a32fc
ziyu jiang <ziyu18895@gmail.com>
2025-10-20 17:08:13 +0200

    afs

--- main.py
+++ main.py
@@ -16,22 +16,13 @@ print("Data base shape: ", df.shape)
 duplicates = df.duplicated().sum()
 print(f"Duplicates found: {duplicates}")
 
-# Data Transformation
-
-df = pd.get_dummies(df, drop_first=True)
-print(df.head())
-
 
 # Select numeric columns (int or float)  #there are also uint8 and category types after get_dummies
-numeric_columns = df.select_dtypes(include=['int64', 'float64','uint8','category']).columns.tolist()
+numeric_columns = df.select_dtypes(include=['int64', 'float64','uint8']).columns.tolist()
 
 # Select non-numeric columns 
-non_numeric_columns = df.select_dtypes(exclude=['int64', 'float64','uint8','category']).columns.tolist()
+non_numeric_columns = df.select_dtypes(exclude=['int64', 'float64','uint8']).columns.tolist()
 
-# Convert all numeric columns to int64
-df= df.astype(int)
-
-print(df.info)
 
 print(" Numerical Columns:")
 for col in numeric_columns:
@@ -41,27 +32,17 @@ print("\n Non-Numerical (Categorical) Columns:")
 for col in non_numeric_columns:
     print(" -", col)
 
+# Data Transformation
+
+df = pd.get_dummies(df, drop_first=True)
+print(df.head())
 
 
-# Standardize numeric columns
 
+# Convert all numeric columns to int64
+df= df.astype(int)
 
-# scaler = StandardScaler()
-# df[numeric_columns] = scaler.fit_transform(df[numeric_columns])
+print(df.info)
 
-# SQL exploratory queries
-pysqldf = lambda q: sqldf(q, globals())
-query1 = """
-SELECT region_1, AVG(credit_amount) AS avg_credit
-FROM df
-GROUP BY region_1
-"""
-result1 = pysqldf(query1)
-print(result1)
-
-plt.bar(result1['region_1'], result1['avg_credit'])
-plt.title('Average Credit Amount by Region')
-plt.xlabel('Regions 1 and 2')
-plt.ylabel('Average Credit Amount')
-plt.show()
-
+df.info()
+print(df.head())
